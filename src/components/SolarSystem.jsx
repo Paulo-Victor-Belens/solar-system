a3fbc30196f6d8f7ad206fa17a597e0a0e511f3f
Paulo Victor Belens Moreira <paulovictor55296@gmail.com>
2023-03-30 15:28:41 -0300

    6º requisito concluido

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 export default class SolarSystem extends Component {
   render() {
@@ -7,6 +9,13 @@ export default class SolarSystem extends Component {
       <section>
         <div data-testid="solar-system"> </div>
         <Title headline="Planetas" />
+        <div>
+          {planets.map(({ name, image }) => (<PlanetCard
+            key={ name }
+            planetName={ name }
+            planetImage={ image }
+          />))}
+        </div>
       </section>
     );
   }
