d592684c894153abe4cd0fc0b1d2a3bda6975987
Paulo Victor Belens Moreira <paulovictor55296@gmail.com>
2023-03-30 15:52:25 -0300

    10º requisito concluido

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 export default class Missions extends Component {
   render() {
@@ -7,6 +9,17 @@ export default class Missions extends Component {
       <section>
         <div data-testid="missions">
           <Title headline="Missões" />
+        </div>
+        <div>
+          {missions.map(({ name, year, country, destination }) => (
+            <MissionCard
+              key={ name }
+              name={ name }
+              year={ year }
+              country={ country }
+              destination={ destination }
+            />
+          ))}
         </div>
       </section>
     );
